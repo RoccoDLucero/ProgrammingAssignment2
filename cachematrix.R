5d34d16899bd3c18b8c92358771328092c3a31cb
RoccoDLucero <rdlucero@bcm.edu>
2016-01-23 22:14:43 -0600

    This was completed by RDL on 1.23.2016

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+#This script contains functions "makeCacheMatrix" and "cacheSolve". 
+#'makeCacheMatrix' creates and updates CacheMatrix objects which store
+#a matrix and its inverse as cached values.
+#'cacheSolve' fetches from cache or computes the inverse of a CacheMatrix object
+#then updates the value of the inverse within the CacheMatrix object
 
-## Write a short comment describing this function
+################################################################################
+#makeCacheMatrix is a function that takes a matrix as input then
+#creates a "Cachematrix" (list) object that stores functions that
+#will store the matrix and its invers as cached values. 
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x){
+    inverse = NULL  #  
+    set.fn <- function(y) {
+        x <<- y     #cache the  matrix value
+        inverse <<- NULL #an update will reset the cached value of inverse
+    }
+    getRaw.fn <- function(){x} 
+    setInverse.fn <- function(inverse){inverse <<- inverse}
+    getInverse.fn <- function(){inverse}
+    list(set = set.fn, getRaw = getRaw.fn,
+         setInverse = setInverse.fn,
+         getInverse = getInverse.fn)
 
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+################################################################################
+#cacheSolve computes the inverse of a "cacheMatrix" object and updates
+#the value of the inverse if the inverse is not cached, otherwise the inverse
+#will be fetched from the cached value.
+cacheSolve <- function(x, ...) {   
+    inverse = x$getInverse()
+    if(!is.null(inverse)) {   
+        message("getting cached data")
+        return(inverse)
+    }
+    
+    #fetch the original matrix data from the "cacheMatrix" object
+    #compute the inverse and update the "cacheMatrix" object in place
+    data = x$getRaw() 
+    inverse = solve(data, ...) 
+    x$setInverse(inverse)      
+    #return(inverse)
 }
+################################################################################
+#Test case:
+#myMatrix = matrix(c(4,3,3,2),nrow = 2) 
+#xx = makeCacheMatrix(myMatrix)
+#xx$getRaw()
+#xx$getInverse()
+#cacheSolve(xx)
+#xx$getInverse()
+################################################################################
